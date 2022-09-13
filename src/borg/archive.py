bcafd3d7fbfa4e903fb1b03cab5925e6486cc65c
Ivan Shapovalov <intelfx@intelfx.name>
2022-09-13 17:32:25 +0300

    extract_item: do not delete an existing directory if possible
    A pre-existing directory might be a Btrfs subvolume that was created by
    the user ahead of time when restoring several nested subvolumes from a
    single archive.

--- src/borg/archive.py
+++ src/borg/archive.py
@@ -803,10 +803,13 @@ Utilization of max. archive size: {csize_max:.0%}
         # Attempt to remove existing files, ignore errors on failure
         try:
             st = os.stat(path, follow_symlinks=False)
-            if stat.S_ISDIR(st.st_mode):
-                os.rmdir(path)
-            else:
+            # remove anything that is not a directory
+            if not stat.S_ISDIR(st.st_mode):
                 os.unlink(path)
+            # only remove a directory if it is conflicting
+            # preserve existing directories because they might be subvolumes
+            elif not stat.S_ISDIR(item.mode):
+                os.rmdir(path)
         except UnicodeEncodeError:
             raise self.IncompatibleFilesystemEncodingError(path, sys.getfilesystemencoding()) from None
         except OSError:
